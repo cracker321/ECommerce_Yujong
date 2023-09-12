f34353153be185285e55e64db159a85db37a5651
yujong <yujong4170@gmail.com>
2023-09-12 15:59:41 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/board/entity/Board.java
+++ src/main/java/yujong/ecommerce_yujong/board/entity/Board.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Auditable;
 @Data
 @NoArgsConstructor
 @Table(name="board")
+@Entity
 public class Board extends Auditable {
 
 
@@ -18,5 +19,39 @@ public class Board extends Auditable {
     private Long boardId;
 
     @Column(length=100, nullable=false)
+    private String title;
+
+    @Column(nullable=false, columnDefinition="TEXT")
+    private String content;
+
+    @Column(nullable=false)
+    private int reviewNum;
+
+    @Column(nullable=false)
+    private double reviewAvg; //double은 정수형 int보다 훨~~씬 넓은 범위의 15~16자리까지의 실수를 표현할 수 있음
+
+    //< 'Board 엔티티'와 'Product 엔티티' 간 일대일 연관 관계. 상품 참조 >
+    private Product product;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
---
+++ src/main/java/yujong/ecommerce_yujong/product/entity/Product.java
@@ -0,0 +1,14 @@
+package yujong.ecommerce_yujong.product.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Table(name="Product")
+@Entity
+public class Product {
+}
