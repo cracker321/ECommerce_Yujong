4d439a12190dafec8e1e20281b666b85502192ef
yujong <yujong4170@gmail.com>
2023-09-12 13:32:14 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/board/controller/BoardController.java
+++ src/main/java/yujong/ecommerce_yujong/board/controller/BoardController.java
@@ -2,11 +2,22 @@ package yujong.ecommerce_yujong.board.controller;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Data
 @RestController
 @RequestMapping("/boards")
-@AllArgsConstructor // 모든 매개변수를
+@AllArgsConstructor //사용자 정의 생성자가 없는 경우에는 '기본 생성자'가 기본적으로 숨겨져 있지만,
+                    //이렇게 @AllArgsConstructor를 사용하여 '모든 매개변수를 포함하는 생성자'를 만드는 경우,
+                    //모든 매개변수를 포함하는 생성자도 어찌되었든 '사용자 정의 생성자'이고, 그에 따라
+                    //아무 매개변수도 들어가 있지 않은 '기본 생성자'를 직접 'public BoardController(){}' 이렇게 생성하든가, 
+                    //아니면, @NoArgsConstructor 를 어노테이션 붙여줘야 한다!
+//@NoArgsConstructor: 매개변수가 없는 기본 생성자를 만들어줌.
+//@RequiredArgsConstructor: //@RequiredArgsConsturctor 어노테이션을 사용하면,
+                            //final로 선언된 필드나 @NonNull 어노테이션을 사용한 필드만을 필요로 하는 생성자를 만들어준다!
+
+//cf)
 public class BoardController {
 }
