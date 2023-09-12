e36604ca517508a87de1f908f8a63e33f5c959c2
yujong <yujong4170@gmail.com>
2023-09-12 12:59:43 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/board/controller/BoardController.java
+++ src/main/java/yujong/ecommerce_yujong/board/controller/BoardController.java
@@ -1,4 +1,12 @@
 package yujong.ecommerce_yujong.board.controller;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/boards")
+@AllArgsConstructor // 모든 매개변수를
 public class BoardController {
 }
