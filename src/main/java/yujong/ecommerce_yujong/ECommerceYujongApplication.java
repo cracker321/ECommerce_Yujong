7cf94e6611c7820009775043ae4950a571270749
yujong <yujong4170@gmail.com>
2023-09-12 12:07:45 +0900

    init

---
+++ src/main/java/yujong/ecommerce_yujong/ECommerceYujongApplication.java
@@ -0,0 +1,13 @@
+package yujong.ecommerce_yujong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceYujongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceYujongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yujong/ecommerce_yujong/ECommerceYujongApplicationTests.java
@@ -0,0 +1,13 @@
+package yujong.ecommerce_yujong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceYujongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
