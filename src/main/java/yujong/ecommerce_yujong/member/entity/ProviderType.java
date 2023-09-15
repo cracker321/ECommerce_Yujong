b0c77225a0ec63ef5f524e2137877a263ea3b1eb
yujong <yujong4170@gmail.com>
2023-09-15 15:05:10 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/member/entity/ProviderType.java
+++ src/main/java/yujong/ecommerce_yujong/member/entity/ProviderType.java
@@ -1,4 +1,12 @@
 package yujong.ecommerce_yujong.member.entity;
 
-public class ProviderType {
+import lombok.Getter;
+
+//enum은 class 대신 들어가는, class 단위이다!
+@Getter
+public enum ProviderType {
+
+    LOCAL,
+    KAKAO,
+    GOOGLe
 }
