65a04cc65fad335a011e75bdf580f4380e295e08
yujong <yujong4170@gmail.com>
2023-09-15 12:35:29 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/review/entity/Review.java
+++ src/main/java/yujong/ecommerce_yujong/review/entity/Review.java
@@ -4,6 +4,9 @@ package yujong.ecommerce_yujong.review.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import yujong.ecommerce_yujong.board.entity.Board;
+import yujong.ecommerce_yujong.member.entity.Customer;
 
 @Data
 @NoArgsConstructor
@@ -22,6 +25,28 @@ public class Review extends Auditable{
 
     @Column(nullable=false)
     private int star;
+
+
+
+    //< Review(N) : Customer(1). N:1 양방향 매핑. 주인객체 Review >
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="customer_id")
+    private Customer customer;
+
+    //*****중요*****
+    //리뷰의 특성 상, 따로 그 Review와 Customer 간 연관관계 메소드를 작성해줄 정도로
+    //리뷰의 속성에 넣어줄 그런 중요한 건 없기 때문에,
+    //Reivew와 Customer 간 연관관계 메소드를 Review 객체의 정보 내부에 넣거나 그러지 않음.
+
+
+
+
+    //< Review(N) : Board(1). N:1 양방향 매핑. 주인객체: Review >
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="board_id")
+    @ToString.Exclude
+    private Board board;
+
 
 
 
