854e0e347dcbe811754da2f7502ec03965d38a87
yujong <yujong4170@gmail.com>
2023-09-20 12:42:34 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/security/config/JwtSecurityConfig.java
+++ src/main/java/yujong/ecommerce_yujong/security/config/JwtSecurityConfig.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import yujong.ecommerce_yujong.security.jwt.SecurityProvider;
+import yujong.ecommerce_yujong.security.jwt.filter.SecurityFilter;
 
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
@@ -13,5 +15,10 @@ public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurity
     private final SecurityProvider securityProvider;
 
 
+    @Override
+    public void configure(HttpSecurity http) {
+        SecurityFilter customFilter = new SecurityFilter(securityProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 
-}
+}   
