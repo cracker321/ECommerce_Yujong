a461509f63a7cbdcfb609267decf49c0a965f712
yujong <yujong4170@gmail.com>
2023-09-20 13:03:56 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/security/config/JwtSecurityConfig.java
+++ src/main/java/yujong/ecommerce_yujong/security/config/JwtSecurityConfig.java
@@ -13,10 +13,17 @@ import yujong.ecommerce_yujong.security.jwt.filter.SecurityFilter;
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     /*
-    [ SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> ]
+    [ SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>{} ]
 
     - 스프링 시큐리티의 기본 설정을 확장하고 사용자 정의 설정을 적용하는 클래스.
-
+    - 비유&실생활 예시
+      * 'SecurityConfigurerAdpater<>{}': 집주인의 입장이 되어, 집 대문의 잠금 장치를 어떠헥 설정할지, 누구에게 열어 줄지 결정하는 역할.
+      * 'DefaultSecurityFilterChain': 집 대문에 설정된 잠금장치 자체를 의미. 스프링 시큐리티가 기본 제공하는 디폴트 필터 체인임.
+      * 'HttpSecurity': 이 잠금장치를 만들 때 필요한 재료, 도구. 즉, SecurityConfig 파일에서 작성한
+                        시큐리티 필터 체인.
+                        특정 Http 요청에 적용할 보안 설정을 구성하는 데 사용되는 것임.
+                        이를 통해, 어떤 사람(요청)이 들어올 수 있고, 어떤 사람이 들어올 수 있는지를 설정하는 것임.
+      
 
      */
     private final SecurityProvider securityProvider;
