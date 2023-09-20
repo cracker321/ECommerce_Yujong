0271aa2f27b1a6d2eca1ed603e575b901b019a56
yujong <yujong4170@gmail.com>
2023-09-20 12:54:27 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/security/config/JwtSecurityConfig.java
+++ src/main/java/yujong/ecommerce_yujong/security/config/JwtSecurityConfig.java
@@ -12,6 +12,13 @@ import yujong.ecommerce_yujong.security.jwt.filter.SecurityFilter;
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
+    /*
+    [ SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> ]
+
+    - 스프링 시큐리티의 기본 설정을 확장하고 사용자 정의 설정을 적용하는 클래스.
+
+
+     */
     private final SecurityProvider securityProvider;
 
 
