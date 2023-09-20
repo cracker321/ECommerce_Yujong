d8d39ee1de5e0e08a4245b8187a0373e695ed716
yujong <yujong4170@gmail.com>
2023-09-20 14:11:59 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/security/config/WebMvcConfig.java
+++ src/main/java/yujong/ecommerce_yujong/security/config/WebMvcConfig.java
@@ -10,8 +10,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
 
+    /*
+
+    [ CORS ]
+
+    https://k3068.tistory.com/51
+
+
+
+
+
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedMethods("POST", "PUT", "GET", "DELETE", "OPTIONS", "PATCH") /* 요청 가능한 메서드 */
+                .allowedHeaders("*") /* 헤더 허용 */
+                .exposedHeaders("Authorization", "Refresh") /* 헤더를 통하여 토큰을 전달해야 하기 때문, 추가 헤더 허용 */
+                .allowedOriginPatterns("*")
+                // .maxAge(3600) /* pre flight 요청에 대한 응답을 캐싱하는 시간 */
+                .allowCredentials(true); /* 쿠키 요청 허용 */
     }
 }
