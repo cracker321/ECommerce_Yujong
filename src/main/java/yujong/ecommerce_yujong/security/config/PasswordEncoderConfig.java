3a415b649e1d7562a5ff25b557bb1c69371e10a5
yujong <yujong4170@gmail.com>
2023-09-23 14:55:53 +0900

    yujong

--- src/main/java/yujong/ecommerce_yujong/security/config/PasswordEncoderConfig.java
+++ src/main/java/yujong/ecommerce_yujong/security/config/PasswordEncoderConfig.java
@@ -8,8 +8,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordEncoderConfig {
-    //순환참조 해결하기 위해 원래는 SecurityConfig 에 있던 아래 부분을 그곳에서 삭제하고
-    //여기에 새롭게 클래스 생성함.
+    //MemberService 클래스와 SecurityConfig 클래스 간 순환참조 해결하기 위해
+    //원래는 SecurityConfig 클래스 내부에 있던 아래 메소드 passwordEncoder를 SecurityConfig 에서 삭제하고
+    //여기에 새롭게 클래스를 생성해서 아래 메소드를 넣음.
 
 
     @Bean
